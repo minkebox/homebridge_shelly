247ae294837c0122fce6d6eda7dbed4417ea9f94
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-05-25 15:54:57 -0700

    Experimental qr codes

--- root/app/configure.sh
+++ root/app/configure.sh
@@ -3,4 +3,5 @@
 cat /app/config.json | sed \
   -e "s/{{BRIDGE_USERNAME}}/${BRIDGE_USERNAME}/g" \
   -e "s/{{BRIDGE_PIN}}/${BRIDGE_PIN}/g" \
+  -e "s/{{BRIDGE_SETUPID}}/${BRIDGE_SETUPID}/g" \
   > /app/homebridge/config.json
